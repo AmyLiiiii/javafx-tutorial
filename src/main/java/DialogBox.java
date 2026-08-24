d387e937070e002dec16cae8032f0120efc3a313
AmyLiiiii <li_zhaohan@u.nus.edu>
2026-08-24 19:28:56 +0800

    Refactor GUI to use FXML

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
+import java.util.Collections;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
@@ -9,28 +14,38 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
 
+/**
+ * Represents a dialog box consisting of an ImageView for the speaker and a label containing text.
+ */
 public class DialogBox extends HBox {
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    public DialogBox(String s, Image i) {
-        text = new Label(s);
-        displayPicture = new ImageView(i);
+    private DialogBox(String text, Image image) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        text.setWrapText(true);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
-        displayPicture.setPreserveRatio(true);
-        double sideLength = Math.min(i.getWidth(), i.getHeight());
+        dialog.setText(text);
+        displayPicture.setImage(image);
+        setRoundAvatar(image);
+    }
+
+    private void setRoundAvatar(Image image) {
+        double sideLength = Math.min(image.getWidth(), image.getHeight());
         displayPicture.setViewport(new Rectangle2D(
-                (i.getWidth() - sideLength) / 2,
-                (i.getHeight() - sideLength) / 2,
+                (image.getWidth() - sideLength) / 2,
+                (image.getHeight() - sideLength) / 2,
                 sideLength,
                 sideLength));
         displayPicture.setClip(new Circle(50.0, 50.0, 50.0));
-        this.setAlignment(Pos.TOP_RIGHT);
-
-        this.getChildren().addAll(text, displayPicture);
     }
 
     /**
@@ -39,17 +54,17 @@ public class DialogBox extends HBox {
     private void flip() {
         this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
+        Collections.reverse(tmp);
         this.getChildren().setAll(tmp);
     }
 
-    public static DialogBox getUserDialog(String s, Image i) {
-        return new DialogBox(s, i);
+    public static DialogBox getUserDialog(String text, Image image) {
+        return new DialogBox(text, image);
     }
 
-    public static DialogBox getSwellDialog(String s, Image i) {
-        DialogBox db = new DialogBox(s, i);
-        db.flip();
-        return db;
+    public static DialogBox getSwellDialog(String text, Image image) {
+        DialogBox dialogBox = new DialogBox(text, image);
+        dialogBox.flip();
+        return dialogBox;
     }
 }
---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,53 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Swell swell;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/Moon.png"));
+    private Image swellImage = new Image(this.getClass().getResourceAsStream("/images/Star.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /**
+     * Injects the Swell instance.
+     */
+    public void setSwell(Swell swell) {
+        this.swell = swell;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Swell's reply, and
+     * then appends them to the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = swell.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getSwellDialog(response, swellImage)
+        );
+        userInput.clear();
+    }
+}
